4126b268435d669c7647282c90cef68b0aff7e0e
Ashymov Aziret <aziretashym@gmail.com>
2024-12-17 19:31:11 +0600

    [API] Server part init.

---
+++ api/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+
+const run = async () => {
+    app.listen(port, () => console.log(`Server started on port http://localhost:${port}`));
+};
+
+run().catch(console.error);
