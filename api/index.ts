2a19baaccd19a1f635f90661cf7ce416b94ab18e
Ashymov Aziret <aziretashym@gmail.com>
2024-12-18 23:17:12 +0600

    [API] Made CRUD, without update for categories.

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import categories_fileDb from "./categories_fileDb";
+import categoryRouter from "./routes/categories/categories";
 
 const app = express();
 const port = 8000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/categories', categoryRouter)
 
 const run = async () => {
+    await categories_fileDb.init()
     app.listen(port, () => console.log(`Server started on port http://localhost:${port}`));
 };
 
---
+++ api/routes/categories/categories.ts
@@ -0,0 +1,51 @@
+import express from "express";
+import categories_fileDb from "../../categories_fileDb";
+import { CategoryWithoutId } from "../../types";
+
+const categoryRouter = express.Router();
+
+categoryRouter.get('/', (_req, res) => {
+    const categories = categories_fileDb.getCategories();
+    res.send(categories);
+});
+
+categoryRouter.get('/:id', async (req, res) => {
+    const {id} = req.params;
+    const category = categories_fileDb.getCategoryById(id);
+
+    if (!category) {
+        res.status(404).send({ error: "Not found" });
+        return;
+    }
+
+    res.send(category);
+})
+
+categoryRouter.post('/', async (req, res) => {
+    if (!req.body.name) {
+        res.status(400).send('Name cannot be empty');
+        return;
+    }
+
+    const ctg: CategoryWithoutId = {
+        name: req.body.name,
+        description: req.body.description,
+    };
+
+    const savedCategory = await categories_fileDb.addCategory(ctg);
+    res.status(201).send(savedCategory);
+});
+
+categoryRouter.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    const success = await categories_fileDb.deleteCategoryById(id);
+
+    if (!success) {
+        res.status(404).send({ error: "Category not found" });
+        return;
+    }
+
+    res.send({ message: "Category deleted successfully" });
+});
+
+export default categoryRouter;
